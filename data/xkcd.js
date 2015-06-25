4b3fe326c5919bc69c342edec795538559fbc029
crudson <github@fishcrime.org>
2015-06-25 01:05:28 -0700

    Shows fixed positioned loading text at top right of page when searching, styled to fit with page. Left pads comicId which gets better search results, and stricter matching on result page.

--- data/xkcd.js
+++ data/xkcd.js
@@ -1,3 +1,24 @@
+self.port.on('addWait', function() {
+  var el = document.createElement('div');
+  el.setAttribute('id', 'xkcd-forum-load');
+  el.style.position = 'fixed';
+  el.style.top = 0;
+  el.style.right = 0;
+  el.textContent = 'Getting forum link...'
+  el.className = 'box';
+  el.style['border-radius'] = '5px';
+  el.style['font-size'] = '12px';
+  el.style.padding = '2px';
+  document.body.appendChild(el);
+});
+
+self.port.on('removeWait', function() {
+  var el = document.getElementById('xkcd-forum-load');
+  if (el) {
+    document.body.removeChild(el);
+  }
+});
+
 // get id for current page
 self.port.on('getComicId', function() {
   var match = null;
@@ -5,7 +26,7 @@ self.port.on('getComicId', function() {
     // either we're looking at current comic, in which case get id from:
     // "Permanent link to this comic: http://xkcd.com/1542/<br />"
     var midEl = document.getElementById('middleContainer');
-    match = midEl.textContent.match(/Permanent link to this comic: http:\/\/xkcd.com\/(\d+)\//);
+    match = midEl.textContent.match(/Permanent link to this comic: http:\/\/xkcd.com\/(\d+)\//i);
   } else {
     // or we're looking at an old comic...
     match = window.location.pathname.match(/^\/(\d+)\/?$/);
@@ -16,7 +37,7 @@ self.port.on('getComicId', function() {
 });
 
 // get link to forum topic from search results markup
-self.port.on('findTopic', function(text) {
+self.port.on('findTopic', function(text, comicId) {
   var parser = new DOMParser();
   var doc = parser.parseFromString(text, "text/html");
   var posts = doc.querySelectorAll('.post');
@@ -24,7 +45,7 @@ self.port.on('findTopic', function(text) {
     var postEl = posts[i];
     if (postEl.querySelector('h3 a span.posthilit')) {
       var topicEl = postEl.querySelector('dd:nth-child(5) a');
-      if (topicEl) {
+      if (topicEl && topicEl.textContent.trim().match(new RegExp('^' + comicId + ':'))) {
         self.port.emit('foundTopicHref', topicEl.getAttribute('href'));
         return;
       }
