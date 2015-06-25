d0cd10703a0b985a6efad4c8b32f4f03a372e414
crudson <github@fishcrime.org>
2015-06-24 22:44:21 -0700

    Supports both current and old comic urls. Button disabled unless viewing comic. If link already added then clicking button uses existing link rather than refetching search page.

--- data/xkcd.js
+++ data/xkcd.js
@@ -1,10 +1,21 @@
+// get id for current page
 self.port.on('getComicId', function() {
-  var match = window.location.pathname.match(/^\/(\d+)\/?$/);
+  var match = null;
+  if (window.location.pathname == '/') {
+    // either we're looking at current comic, in which case get id from:
+    // "Permanent link to this comic: http://xkcd.com/1542/<br />"
+    var midEl = document.getElementById('middleContainer');
+    match = midEl.textContent.match(/Permanent link to this comic: http:\/\/xkcd.com\/(\d+)\//);
+  } else {
+    // or we're looking at an old comic...
+    match = window.location.pathname.match(/^\/(\d+)\/?$/);
+  }
   if (match) {
     self.port.emit('foundComicId', match[1]);
   }
 });
 
+// get link to forum topic from search results markup
 self.port.on('findTopic', function(text) {
   var parser = new DOMParser();
   var doc = parser.parseFromString(text, "text/html");
@@ -22,11 +33,23 @@ self.port.on('findTopic', function(text) {
   console.log('no match');
 });
 
+// add a link to forum to current page
 self.port.on('addLink', function(href) {
   var midEl = document.getElementById('middleContainer');
   var aEl = document.createElement('a');
+  aEl.setAttribute('id', 'xkcd-forum-link');
   aEl.href = href;
   aEl.textContent = 'Forum Link';
   midEl.appendChild(document.createElement('br'));
   midEl.appendChild(aEl);
+});
+
+// if we previously added a forum link report it, otherwise report back that we didn't
+self.port.on('findExistingLink', function() {
+  var aEl = document.getElementById('xkcd-forum-link');
+  if (aEl) {
+    self.port.emit('existingTopicHrefFound', aEl.href);
+  } else {
+    self.port.emit('existingTopicHrefNotFound');
+  }
 });
